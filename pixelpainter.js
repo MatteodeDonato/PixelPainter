5df4b8f6d66c1fbf5b22aab7d497f34fb1e5274b
Emily Mattlin <emilyjmattlin@gmail.com>
2016-02-26 11:00:06 -0500

    Update pixelpainter.js

--- pixelpainter.js
+++ pixelpainter.js
@@ -1,20 +1,60 @@
-function setup(){
-    createCanvas(500,300);
-    fill(255);
+// Learning Processing
+// Daniel Shiffman
+// http://www.learningprocessing.com
+// Example 1-1: stroke and fill
+var x = 0;
+var y = 0;
+var pencilSelected = 0;
+var markerSelected = 0;
+function setup() {
+  frameRate(10000);
+  createCanvas(960, 540);
+  fill(0, 255, 0);
+  stroke(255,0,0);
+  background(255);
 }
-function draw(){
-    if(mouseIsPressed){
-       if(mouseButton== LEFT){
-        stroke(255,0,0);
-        line(mouseX,mouseY,pmouseX,pmouseY);
-       }
-       if(mouseButton == RIGHT){
-        stroke(0,255,0);
-        line(mouseX,mouseY,pmouseX,pmouseY);
-       }
-       
+function mouseClicked(){
+    if(dist(mouseX, mouseY, 50, 50)<=25){
+        if(pencilSelected==0){
+            pencilSelected = 1;
+            markerSelected= 0;
+        }
     }
-    if(keyIsPressed){
-        clear();
+    if(dist(mouseX, mouseY, 50, 150)<=25){
+        if(markerSelected==0){
+            pencilSelected = 0;
+            markerSelected= 1;
+        }
     }
 }
+function draw() {
+    if(pencilSelected==1){
+    if (mouseIsPressed){
+      line(pmouseX, pmouseY, mouseX, mouseY);
+    if(abs(mouseX-pmouseX)<=0 && abs(mouseY-pmouseY)<=0){
+     point(mouseX, mouseY);   
+    }
+    }
+    }
+    if(markerSelected==1){
+    if (mouseIsPressed){
+        strokeWeight(10);
+      line(pmouseX, pmouseY, mouseX, mouseY);
+    if(abs(mouseX-pmouseX)<=0 && abs(mouseY-pmouseY)<=0){
+     point(mouseX, mouseY);   
+    }
+    }
+    }
+  strokeWeight(1);
+  ellipse(50, 50, 50, 50);
+  ellipse(50, 150, 50, 50);
+//  if (mouseIsPressed){
+//    if(dist((mouseX, mouseY, width-50, 50)<=25){
+//      line(pmouseX, pmouseY, mouseX, mouseY);
+//    }
+//}
+//function mouseClicked() {
+  //if(dist(mouseX, mouseY, width-50, 50)<=25){
+   //}
+}
+
