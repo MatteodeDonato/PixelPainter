0dfc4f42026c2c33ea89213b458116c6efc38d38
Emily Mattlin <emilyjmattlin@gmail.com>
2016-02-29 12:14:17 -0500

    Update pixelpainter.js
    paintbrush added-opacity adjusted and blurry

--- pixelpainter.js
+++ pixelpainter.js
@@ -1,14 +1,16 @@
+// Learning Processing
+// Daniel Shiffman
+// http://www.learningprocessing.com
+// Example 1-1: stroke and fill
 var x = 0;
 var y = 0;
 var pencilSelected = 0;
 var markerSelected = 0;
-
-
-    
+var paintbrushSelected = 0;
 function setup() {
   frameRate(10000);
   createCanvas(960, 540);
-  fill(0, 255, 0);
+  fill(255, 0, 0);
   stroke(255,0,0);
   background(255);
 }
@@ -17,33 +19,25 @@ function mouseClicked(){
         if(pencilSelected==0){
             pencilSelected = 1;
             markerSelected= 0;
+            paintbrushSelected= 0;
         }
     }
     if(dist(mouseX, mouseY, 50, 150)<=25){
         if(markerSelected==0){
             pencilSelected = 0;
             markerSelected= 1;
-        } }
-        if(dist(mouseX, mouseY, 150, 50)<=25){
-            Colorcurrent==Colors.Red;
+            paintbrushSelected= 0;
+        }
     }
-        else if(dist(mouseX, mouseY, 250, 50)<=25){
-            Colorcurrent==Colors.Green;
+    if(dist(mouseX, mouseY, 50, 250)<=25){
+        if(markerSelected==0){
+            pencilSelected = 0;
+            markerSelected= 0;
+            paintbrushSelected= 1;
+        }
     }
-        else if(dist(mouseX, mouseY, 350, 50)<=25){
-            Colorcurrent==Colors.Blue;
-    }
-
 }
 function draw() {
-    /*if(Colorcurrent>-1){
-        if(Colorcurrent==0)
-            fill(Color.red);
-        if(Colorcurrent==1)
-            fill(Color.green);
-        if(Colorcurrent==2)
-            fill(Color.blue);
-    }*/
     if(pencilSelected==1){
     if (mouseIsPressed){
       line(pmouseX, pmouseY, mouseX, mouseY);
@@ -57,20 +51,28 @@ function draw() {
         strokeWeight(10);
       line(pmouseX, pmouseY, mouseX, mouseY);
     if(abs(mouseX-pmouseX)<=0 && abs(mouseY-pmouseY)<=0){
-     point(mouseX, mouseY);   
+       point(mouseX, mouseY);   
     }
     }
     }
-    if(keyIsPressed){
-        clear();
+    if(paintbrushSelected==1){
+    if (mouseIsPressed){
+        stroke(255,0,0,80);
+        for(var i =0; i<10; i++){
+        strokeWeight(3*i);
+        //ellipse(mouseX, mouseY, 15, 15);
+      ellipse(mouseX, mouseY, 15, 15);
+        }
+    if(abs(mouseX-pmouseX)<=0 && abs(mouseY-pmouseY)<=0){
+       ellipse(mouseX, mouseY, 10, 10);   
     }
-  strokeWeight(1);
+    }
+    }
+  strokeWeight(1); 
   ellipse(50, 50, 50, 50);
   ellipse(50, 150, 50, 50);
-  ellipse(150, 50, 50, 50);
-  ellipse(250, 50, 50, 50);
-  ellipse(350, 50, 50, 50);
-    //  if (mouseIsPressed){
+  ellipse(50, 250, 50, 50);
+//  if (mouseIsPressed){
 //    if(dist((mouseX, mouseY, width-50, 50)<=25){
 //      line(pmouseX, pmouseY, mouseX, mouseY);
 //    }
@@ -79,4 +81,3 @@ function draw() {
   //if(dist(mouseX, mouseY, width-50, 50)<=25){
    //}
 }
-
