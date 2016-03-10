8fddcad08493455f80cdd25cdd4a0278f2bf9500
Matteo de Donato <MatteodeDonato@users.noreply.github.com>
2016-03-10 12:02:07 -0500

    Update pixelpainter.js
    add tools to nav bar

--- pixelpainter.js
+++ pixelpainter.js
@@ -1,83 +1,89 @@
-// Learning Processing
-// Daniel Shiffman
-// http://www.learningprocessing.com
-// Example 1-1: stroke and fill
 var x = 0;
 var y = 0;
-var pencilSelected = 0;
-var markerSelected = 0;
-var paintbrushSelected = 0;
+var mode = "none";
+var backColor = 255;
 function setup() {
   frameRate(10000);
-  createCanvas(960, 540);
-  fill(26,188,156);
-  stroke(26,188,156);
+  createCanvas(windowWidth, windowHeight);
+  fill(255, 0, 0);
+  stroke(255,0,0);
   background(255);
 }
-function mouseClicked(){
-    if(dist(mouseX, mouseY, 50, 50)<=25){
-        if(pencilSelected==0){
-            pencilSelected = 1;
-            markerSelected= 0;
-            paintbrushSelected= 0;
-        }
-    }
-    if(dist(mouseX, mouseY, 50, 150)<=25){
-        if(markerSelected==0){
-            pencilSelected = 0;
-            markerSelected= 1;
-            paintbrushSelected= 0;
-        }
-    }
-    if(dist(mouseX, mouseY, 50, 250)<=25){
-        if(markerSelected==0){
-            pencilSelected = 0;
-            markerSelected= 0;
-            paintbrushSelected= 1;
-        }
-    }
-}
+
+
+function makePencil(){mode = "pencil";}
+function makePaint(){mode = "paintbrush";}
+function makeEraser(){mode = "eraser"};
+function makeSpray(){mode = "sprayCan"};
+function makeMarker(){mode = "marker"};
+function makeLine(){mode = "line"};
+
 function draw() {
-    if(pencilSelected==1){
+    if(mode=="pencil"){
     if (mouseIsPressed){
       line(pmouseX, pmouseY, mouseX, mouseY);
     if(abs(mouseX-pmouseX)<=0 && abs(mouseY-pmouseY)<=0){
-     point(mouseX, mouseY);
+     point(mouseX, mouseY);   
     }
     }
     }
-    if(markerSelected==1){
+    if(mode=="marker"){
     if (mouseIsPressed){
         strokeWeight(10);
       line(pmouseX, pmouseY, mouseX, mouseY);
     if(abs(mouseX-pmouseX)<=0 && abs(mouseY-pmouseY)<=0){
-       point(mouseX, mouseY);
+       point(mouseX, mouseY);   
     }
     }
     }
-    if(paintbrushSelected==1){
+    if(mode=="paintbrush"){
     if (mouseIsPressed){
-        stroke(26,188,156, 80);
-        for(var i =0; i<10; i++){
+        for(var i =10; i>00; i--){
+            stroke(255,0,0,35);
+        strokeCap(ROUND);
+        line(pmouseX, pmouseY, mouseX, mouseY);
         strokeWeight(3*i);
-        //ellipse(mouseX, mouseY, 15, 15);
-      ellipse(mouseX, mouseY, 15, 15);
+            stroke(255,0,0,35);
+        strokeCap(SQUARE);
+      line(pmouseX, pmouseY, mouseX, mouseY);
         }
     if(abs(mouseX-pmouseX)<=0 && abs(mouseY-pmouseY)<=0){
-       ellipse(mouseX, mouseY, 10, 10);
+       //ellipse(mouseX, mouseY, 15, 15);   
     }
     }
     }
-  strokeWeight(1);
-  ellipse(50, 50, 50, 50);
-  ellipse(50, 150, 50, 50);
-  ellipse(50, 250, 50, 50);
-//  if (mouseIsPressed){
-//    if(dist((mouseX, mouseY, width-50, 50)<=25){
-//      line(pmouseX, pmouseY, mouseX, mouseY);
-//    }
-//}
-//function mouseClicked() {
-  //if(dist(mouseX, mouseY, width-50, 50)<=25){
-   //}
+    if(mode=="eraser"){
+    if (mouseIsPressed){
+        strokeWeight(50);
+        stroke(backColor);
+        line(pmouseX, pmouseY, mouseX, mouseY);
+    if(abs(mouseX-pmouseX)<=0 && abs(mouseY-pmouseY)<=0){
+       point(mouseX, mouseY);   
+    }
+    }
+        stroke(255, 0, 0);
+    }
+    if(mode=="sprayCan"){
+        strokeWeight(1); 
+    if (mouseIsPressed){
+      for (var h = 0; h < 100; h++) {  
+        var x = randomGaussian(mouseX,15);
+        var y = randomGaussian(mouseY,15);
+          for(var i = 0; i<20; i++){
+            point(x, y);
+          }
+      }
+    if(abs(mouseX-pmouseX)<=0 && abs(mouseY-pmouseY)<=0){
+       point(mouseX, mouseY);   
+    }
+    }
+    }
+    if(mode=="line"){
+        strokeWeight(5); 
+    if (mouseClicked){
+        var x =pmouseX;
+        var y =pmouseY;
+        line(x, y, mouseX, mouseY);
+    }
+    }
 }
