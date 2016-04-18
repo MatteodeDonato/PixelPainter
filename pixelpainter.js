e5a27821e447ee7386d8aa525acc67c1d7e5aa5c
Matteo de Donato <MatteodeDonato@users.noreply.github.com>
2016-04-18 12:11:22 -0400

    Update pixelpainter.js

--- pixelpainter.js
+++ pixelpainter.js
@@ -127,7 +127,7 @@ function saveImg(fileType) {
 
 function draw() {
 
-    document.getElementById("p1").innerHTML = "(" + mouseX + "," + mouseY + ") ";
+    document.getElementById("cursorTracker").innerHTML = "      (" + mouseX + "," + mouseY + ") ";
     r1 = r1Slider.value();
     g1 = g1Slider.value();
     b1 = b1Slider.value();
